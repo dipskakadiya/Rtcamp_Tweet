c2a084220670f3d62d25ad11ed5c5d8847d4d0eb
Dipesh <dipesh.kakadiya111@gmail.com>
2015-05-23 14:28:52 +0530

    update credential

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 
 /* configuration */
 
-define('CONSUMER_KEY', '12IOhu1U7Sdj9DGExkhvqdoIe');
-define('CONSUMER_SECRET', 'Osw6SumO15DjpXfJXcBwkF8SFrzGTm1KaZoJZF5MftjsxN3mTp');
+define('CONSUMER_KEY', '3JyT5dri4wueniED9CDyyrtWT');
+define('CONSUMER_SECRET', '1InH04IYjtbFrrkVpmqxERcmXX788WVzYtUVC9HOWu0iM55WId');
 define('OAUTH_CALLBACK', 'http://rtcamptweet-dips.rhcloud.com/redirect.php'); //for localhost
 
 
